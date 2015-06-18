434c9481dbcca2a04a4d12f326c47d112335997c
Jim <jim.lemmers@gmail.com>
2015-06-18 16:33:32 +0200

    added runme to convert which will generate all the json files

---
+++ convert/runme.py
@@ -0,0 +1,21 @@
+import glob, os
+from v3 import *
+
+
+json_dir = 'data/json/'
+if not os.path.exists(json_dir):
+    print('creating the json dir')
+    os.makedirs(json_dir)
+
+filelist = glob.glob('convert/data/json/*.json')
+
+if filelist:
+    for f in filelist:
+        os.remove(f)
+
+# Dictionary of correct variables for all years.
+print('creating normal json')
+write_json(variables, inp_dir_prefix='', outdir='data/json/')
+print('creating tree json')
+write_json(variables, tree=False,
+            inp_dir_prefix='', outdir='data/json/')
